045dd76c42b4e35de940c0885d7f63eb2916c53c
Ewa Milczanowska (PGS Software) <emilczanowska@pgs-soft.com>
2018-09-20 13:58:20 +0200

    Java Koans exercises - DONE

---
+++ src/beginner/AboutKoans.java
@@ -0,0 +1,25 @@
+package beginner;
+
+import com.sandwich.koan.Koan;
+
+import static com.sandwich.util.Assert.fail;
+
+public class AboutKoans {
+
+    @Koan
+    public void findAboutKoansFile() {
+        boolean koanIsComplete = true;
+        if(!koanIsComplete) {
+            fail("delete this line to advance");
+        }
+    }
+
+    @Koan
+    public void definitionOfKoanCompletion() {
+        boolean koanIsComplete = true;
+        if (!koanIsComplete) {
+            fail("what if koanIsComplete variable was true?");
+        }
+    }
+
+}
